d3bc36e0c4ae0c0458a039937c89be55b527f0f0
Cihancelikoglu <CihanCelikoglu2@hotmail.com>
2022-09-03 19:05:16 +0300

    Entity Framework

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,18 @@
 ﻿using Business.Concrete;
+using DataAccsess.Concrete.EntityFramework;
 using DataAccsess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //S -O- LID
+    //Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var item in productManager.GetAll())
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(item.ProductName);
             }
--- DataAccsess/Abstract/IEntityRepository.cs
+++ DataAccsess/Abstract/IEntityRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccsess.Abstract
 {
-    public interface IEntityRepository<T>
+    //Generic Constraint
+    //class : referans tip olabilir
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new() : new'lenebilir olmalı
+
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
